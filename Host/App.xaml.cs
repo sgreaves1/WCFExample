ba7bf761234611dfa1ee6b64e098ce742388b3fc
unknown <SamG@DEV-21.Meyertech.local>
2017-04-13 14:15:17 +0100

    Renamed main project

--- Host/App.xaml.cs
+++ Host/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WCF_Example
+namespace Host
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Host/MainWindow.xaml.cs
+++ Host/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WCF_Example
+namespace Host
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
