a17fc300cf901f163632a8d1752e4e0809f2ae36
unknown <SamG@DEV-21.Meyertech.local>
2017-04-13 14:44:08 +0100

    Open a service in host app

--- Host/ViewModel/MainWindowViewModel.cs
+++ Host/ViewModel/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ServiceModel;
+using System.ServiceModel.Description;
 using AlarmServiceLibrary;
 
 namespace Host.ViewModel
@@ -12,7 +13,26 @@ namespace Host.ViewModel
             Uri baseAddress = new Uri("http://localhost:9000/GettingStarted/");
 
             // Step 2 Create a ServiceHost instance  
-            ServiceHost selfHost = new ServiceHost(typeof(AlarmService), baseAddress);
+            ServiceHost selfHost = new ServiceHost(typeof (AlarmService), baseAddress);
+
+            try
+            {
+                // Step 3 Add a service endpoint.  
+                selfHost.AddServiceEndpoint(typeof (IAlarmService), new WSHttpBinding(), "AlarmService");
+
+                // Step 4 Enable metadata exchange.  
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                // Step 5 Start the service.  
+                selfHost.Open();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine(@"An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
         }
     }
 }
