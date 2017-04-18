ea73a8482cb4e96c6f58f8d9aaf4fe2ef6b019d6
unknown <SamG@DEV-21.Meyertech.local>
2017-04-18 11:22:28 +0100

    Alarm details now show when alarm selected

--- Host/UserControl/AlarmDetails.xaml.cs
+++ Host/UserControl/AlarmDetails.xaml.cs
@@ -1,13 +1,28 @@
-﻿namespace Host.UserControl
+﻿using System.Windows;
+using Host.Model;
+
+namespace Host.UserControl
 {
     /// <summary>
     /// Interaction logic for AlarmDetails.xaml
     /// </summary>
     public partial class AlarmDetails
     {
+        public static readonly DependencyProperty AlarmProperty =
+            DependencyProperty.Register("Alarm", 
+                typeof(AlarmModel), 
+                typeof(AlarmDetails), 
+                new PropertyMetadata(null));
+
         public AlarmDetails()
         {
             InitializeComponent();
+        }
+
+        public AlarmModel Alarm
+        {
+            get { return (AlarmModel)GetValue(AlarmProperty); }
+            set { SetValue(AlarmProperty, value); }
         }
     }
 }
