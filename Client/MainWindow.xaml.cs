afde54d085e90292a05a88c5569ccf9c02c75a45
unknown <SamG@DEV-21.Meyertech.local>
2017-04-18 14:44:03 +0100

    Add main window view model

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Client.AlarmServiceReference;
+using Client.ViewModel;
 
 namespace Client
 {
@@ -33,6 +23,8 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainWindowViewModel();
 
             Console.WriteLine("Client id: " + clientID);
             Console.WriteLine("The client is running.");
