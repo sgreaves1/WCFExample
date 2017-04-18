8c671c2f1cda8fe1cca404198dc79e132e1a7e90
unknown <SamG@DEV-21.Meyertech.local>
2017-04-18 14:58:21 +0100

    Move all code from client backend to viewmodel

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -13,60 +13,12 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static Random rnd = new Random(DateTime.Now.Millisecond);
-        private static int clientID = rnd.Next(1, 10000);
-
-        static CancellationTokenSource cancellation = new CancellationTokenSource();
-
-        static AlarmServiceClient client = new AlarmServiceClient();
-
         public MainWindow()
         {
             InitializeComponent();
 
             DataContext = new MainWindowViewModel();
-
-            Console.WriteLine("Client id: " + clientID);
-            Console.WriteLine("The client is running.");
-            Console.WriteLine("The client will send random alarms ever second.");
-            Console.WriteLine();
-            Run();
-        }
-
-        static async void Run()
-        {
-            await RepeatActionEvery(Action, TimeSpan.FromSeconds(1), cancellation.Token);
-        }
-
-        private static void Action()
-        {
-            client.ActivateAlarm(clientID, "Sam");
-            Console.WriteLine("Alarm Sent.");
-        }
-
-        public static async Task RepeatActionEvery(Action action, TimeSpan interval, CancellationToken cancellationToken)
-        {
-            while (true)
-            {
-                action();
-                Task task = Task.Delay(interval, cancellationToken);
-
-                try
-                {
-                    await task;
-                }
-
-                catch (TaskCanceledException)
-                {
-                    Console.WriteLine("Alarm Task Cancel");
-                    return;
-                }
-            }
-        }
-
-        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
-        {
-            client.Close();
+            
         }
     }
 }
--- Client/ViewModel/MainWindowViewModel.cs
+++ Client/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,58 @@
-﻿namespace Client.ViewModel
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Client.AlarmServiceReference;
+
+namespace Client.ViewModel
 {
     public class MainWindowViewModel : BaseModel
     {
+        private static Random rnd = new Random(DateTime.Now.Millisecond);
+        private static int clientID = rnd.Next(1, 10000);
+        
+        static CancellationTokenSource cancellation = new CancellationTokenSource();
+
+        static AlarmServiceClient client = new AlarmServiceClient();
+
+        public MainWindowViewModel()
+        {
+            Run();
+        }
+
+        ~MainWindowViewModel()
+        {
+            client.Close();
+        }
+
+        static async void Run()
+        {
+            await RepeatActionEvery(Action, TimeSpan.FromSeconds(1), cancellation.Token);
+        }
+
+        private static void Action()
+        {
+            client.ActivateAlarm(clientID, "Sam");
+            Console.WriteLine("Alarm Sent.");
+        }
+
+        public static async Task RepeatActionEvery(Action action, TimeSpan interval, CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                action();
+                Task task = Task.Delay(interval, cancellationToken);
+
+                try
+                {
+                    await task;
+                }
+
+                catch (TaskCanceledException)
+                {
+                    Console.WriteLine("Alarm Task Cancel");
+                    return;
+                }
+            }
+        }
     }
 }
