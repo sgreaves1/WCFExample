032220ff716fd3869daac85c02bcbd0841026f8a
unknown <SamG@DEV-21.Meyertech.local>
2017-04-19 15:39:12 +0100

    Add service model to client

---
+++ Client/Model/ServiceModel.cs
@@ -0,0 +1,39 @@
+﻿namespace Client.Model
+{
+    public class ServiceModel : BaseModel
+    {
+        private string _name;
+        private string _endpointAddress;
+        private bool _connected;
+
+        public string Name
+        {
+            get {  return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string EndpointAddress
+        {
+            get { return _endpointAddress; }
+            set
+            {
+                _endpointAddress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Connected
+        {
+            get { return _connected; }
+            set
+            {
+                _connected = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
